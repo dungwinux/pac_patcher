f358660c08e30bd051ec140212da0573d4b197a7
Dung Tuan Nguyen <ntddebugger@gmail.com>
2022-05-20 08:22:00 +0700

    fix: continue patching when offset change

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /**
  * The MIT License (MIT)
  * Copyright © 2022 Dung Tuan Nguyen
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
@@ -69,23 +69,23 @@ function encodeSrp(file) {
 offset_change = 0;
 parsed.chunks.forEach((chunk) => {
     const patched_file = chunk.name + ".srp";
-    if (offset_change !== 0) {
-        chunk.fileContent = new CraftedChunk(
-            chunk.fileContent.data,
-            chunk.fileContent.relOffset + offset_change,
-            chunk.fileContent.offsetSize
-        );
-    }
+
     // Uncomment to backup original file
     // fs.writeFileSync(patched_file + ".bak", decode(chunk.fileContent.data));
     if (chunk.fileContent.cat == "srp" && fs.existsSync(patched_file)) {
         console.log(`Modifying ${chunk.name} using ${chunk.name}.srp`);
         const patched_data = encodeSrp(`${chunk.name}.srp`);
-        offset_change = patched_data.byteLength - chunk.fileContent.offsetSize;
         chunk.fileContent = new CraftedChunk(
             patched_data,
-            chunk.fileContent.relOffset,
+            offset_change + chunk.fileContent.relOffset,
             patched_data.byteLength
+        );
+        offset_change += patched_data.byteLength - chunk.fileContent.offsetSize;
+    } else if (offset_change !== 0) {
+        chunk.fileContent = new CraftedChunk(
+            chunk.fileContent.data,
+            chunk.fileContent.relOffset + offset_change,
+            chunk.fileContent.offsetSize
         );
     }
 });
